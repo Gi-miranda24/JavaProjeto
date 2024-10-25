d89924fcf56e60c927bc921967e9498d3b955f6e
Giselle <gi.miranda2424@gmail.com>
2024-10-25 10:47:08 -0300

    add a validação do usuario

--- src/produtosnaturais/Usuario.java
+++ src/produtosnaturais/Usuario.java
@@ -14,10 +14,14 @@ public class Usuario {
     private String nome;
     private String email;
     private String senha;
-    private String telefone;
+    private double telefone;
     List<Produto> produtos;
     
-    public Usuario(String nome, String email, String senha, String telefone) {
+    public Usuario(String nome, String email, String senha, double telefone) {
+        validarNome(nome);//Validação ocorre aqui
+        validarEmail(email);//Validação ocorre aqui
+        validarSenha(senha);//Validação ocorre aqui
+        validarTelefone(telefone);//Validação ocorre aqui
         this.nome = nome;
         this.email = email;
         this.senha = senha;
@@ -51,15 +55,40 @@ public class Usuario {
         this.senha = senha;
     }
 
-    public String getTelefone() {
+    public double getTelefone() {
         return telefone;
     }
 
-    public void setTelefone(String telefone) {
+    public void setTelefone(double telefone) {
         this.telefone = telefone;
+    }
+    
+    //Validações especificas para Usuario
+    protected void validarNome(String nome){
+        if(nome == null || nome.trim(). isEmpty()){
+            throw new IllegalArgumentException("O  campo nome não pode estar vazio.");
+   
+        }
+    }
+    protected void validarEmail(String email){
+        if(email == null || email.trim(). isEmpty()){
+            throw new IllegalArgumentException("o Campo email não pode estar vazia.");
+   
+        }
     }
     
     
     
-    
+    protected void validarSenha(String senha){
+        if(senha == null || senha.trim(). isEmpty()){
+            throw new IllegalArgumentException("O  campo senha não pode estar vazio.");
+   
+        }
+    }
+    protected void validarTelefone(double telefone){
+        if(telefone == 0){
+            throw new IllegalArgumentException("o Campo telefone não pode estar vazia.");
+   
+        }
+    }
 }
